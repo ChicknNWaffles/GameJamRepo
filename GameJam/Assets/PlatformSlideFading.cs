2e711d2d35383853dd9694da5008f807368cdd23
Matthew Christensen <christensenfamily05@gmail.com>
2025-04-11 21:50:42 -0500

    Initial Platforms

---
+++ GameJam/Assets/PlatformSlideFading.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSlideFading : PlatformSlide
+{
+    public bool fadeOverTime;
+    public float holdFade = 1f;
+    public float fadeAmount = 0f;
+    public float fadeSpeed = 1f;
+
+    public bool mouseDebug = false;
+
+    //bool wasFading = false;
+    bool fading = false;
+    float fadeEnd;
+
+    Collider2D col;
+
+    SpriteRenderer sprite;
+    Color tmp;
+
+    private void OnFaded()
+    {
+        col.isTrigger = true;
+    }
+
+    private void OnUnfadeStart()
+    {
+
+    }
+
+    private void OnUnfadeEnd()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
+
+    private void StartFading()
+    {
+        if (fadeAmount == 1f) return;
+
+        fadeAmount = Mathf.MoveTowards(fadeAmount, 1f, fadeSpeed * Time.deltaTime);
+        fading = true;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        if (!fadeOverTime)
+        {
+            if (fading)
+            {
+                fadeAmount = Mathf.MoveTowards(fadeAmount, 1f, fadeSpeed * Time.deltaTime);
+
+                if (fadeAmount == 1f)
+                {
+                    fading = false;
+                    fadeEnd = Time.time + holdFade;
+                    OnFaded();
+                }
+            }
+            else if (Time.time > fadeEnd)
+            {
+                fadeAmount = Mathf.MoveTowards(fadeAmount, 0f, fadeSpeed * Time.deltaTime);
+                col.isTrigger = false;
+            }
+        }
+        else
+        {
+            if (fading && fadeAmount == 1f)
+            {
+                fading = false;
+                fadeEnd = Time.time + holdFade;
+                OnFaded();
+            }
+
+            if (fading == false && (fadeAmount != 1f || Time.time > fadeEnd))
+            { 
+                fadeAmount = Mathf.MoveTowards(fadeAmount, 0f, fadeSpeed * Time.deltaTime);
+                col.isTrigger = false;
+            }
+
+            fading = false;
+        }
+
+        tmp = sprite.color;
+        tmp.a = 1f - fadeAmount;
+        sprite.color = tmp;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!fadeOverTime && !fading && collision.gameObject.tag == "Player")
+        {
+            fading = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player") return;
+
+        if (fadeOverTime)
+        {
+            StartFading();
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (!mouseDebug) return;
+
+        
+
+        if (fadeOverTime)
+        {
+            StartFading();
+        }
+
+        fading = true;
+    }
+}
