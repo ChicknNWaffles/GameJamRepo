4a326d55f062b0b70e82248ecd2d09fd7990bbad
ChicknNWaffles <elora.browning@gmail.com>
2025-04-12 00:07:49 -0500

    Collision now disables movement
    undtested

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; } = null;
     private bool Jumping;
+    private bool Mode; // false = side, true = top
     private float Speed;
-
+    private bool CanGoLeft = true;
+    private bool CanGoRight = true;
+    private bool CanGoUp = true;
+    private bool CanGoDown = true;
+    private bool CanJump = true;
+    private Vector3 position;
+    private int health;
 
     private void Awake() {
         if (Instance == null) {
@@ -16,8 +24,8 @@ public class Player : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        position = gameObject.transform.position;
         
     }
 
@@ -47,6 +55,60 @@ public class Player : MonoBehaviour
 
     void Crouch() { 
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "collide") {
+            // if colliding on the left
+            if (position.x <= collision.gameObject.transform.position.x) {
+                // disable movement to the left
+                CanGoLeft = false;
+            }
+            // if colliding on the right
+            else if (position.x > collision.gameObject.transform.position.x) {
+                // disable movement to the right
+                CanGoRight = false;
+            }
+            // if colliding above
+            else if (position.y <= collision.gameObject.transform.position.y) {
+                // disable movement upward
+                CanGoUp = false;
+                // disable jumping
+                CanJump = false;
+            }
+            // if colliding below
+            else if (position.y > collision.gameObject.transform.position.y) {
+                // disable movement downward
+                CanGoDown = false;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.tag == "collide") {
+            // if colliding on the left
+            if (position.x <= collision.gameObject.transform.position.x) {
+                //enable movement to the left
+                CanGoLeft = true;
+            }
+            // if colliding on the right
+            else if (position.x > collision.gameObject.transform.position.x) {
+                //enable movement to the right
+                CanGoRight = true;
+            }
+            // if colliding above
+            else if (position.y <= collision.gameObject.transform.position.y) {
+                //enable movement upward
+                CanGoUp = true;
+                //enable jumping
+                CanJump = true;
+            }
+            // if colliding below
+            else if (position.y > collision.gameObject.transform.position.y) {
+                //enable movement downward
+                CanGoDown = true;
+            }
+        }
     }
 
 
