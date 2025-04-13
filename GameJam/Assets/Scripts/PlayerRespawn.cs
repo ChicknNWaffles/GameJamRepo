9686e388381c78641c73a764b23ec2aa14850033
Matthew Christensen <christensenfamily05@gmail.com>
2025-04-13 11:22:56 -0500

    Update PlayerRespawn.cs

--- GameJam/Assets/Scripts/PlayerRespawn.cs
+++ GameJam/Assets/Scripts/PlayerRespawn.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
+    Vector3 respawnPoint;
+    Health health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = GetComponent<Health>();
+        respawnPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Respawn()
+    {
+        transform.position = respawnPoint;
+        
+        if (health != null)
+        {
+            health.ResetHealth();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Checkpoint")
+        {
+            respawnPoint = collision.transform.position;
+        }
     }
 }
