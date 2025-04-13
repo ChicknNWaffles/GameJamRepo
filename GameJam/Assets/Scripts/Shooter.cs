95c00d4632a109773faa1ca6367bedf975b57b96
Matthew Christensen <christensenfamily05@gmail.com>
2025-04-12 19:05:44 -0500

    Shooting and Platform fixes

--- GameJam/Assets/Scripts/Shooter.cs
+++ GameJam/Assets/Scripts/Shooter.cs
@@ -6,7 +6,11 @@ public class Shooter : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public Transform shootTransform;
+    public Vector3 shootDir = new Vector2(0.819152f, 0.573576f);
     public float shootDelay = .7f;
+    public float shootForce = 10f;
+
+    Vector3 shootTemp;
 
     float nextShoot;
 
@@ -21,10 +25,14 @@ public class Shooter : MonoBehaviour
     void Shoot()
     {
         if (Time.time < nextShoot) return;
+
+        shootTemp = transform.localScale.x < 0f ? new Vector3(-shootDir.x, shootDir.y) : shootDir;
+
         nextShoot = Time.time + shootDelay;
 
         var projectile = Instantiate(projectilePrefab);
         projectile.transform.position = shootTransform.position;
+        projectile.GetComponent<Rigidbody2D>().AddForce(shootTemp * shootForce, ForceMode2D.Impulse);
     }
 
     // Start is called before the first frame update
